58498e613277cc8fc729c066cf79202c5c54f0b8
roast000 <107483506+roast000@users.noreply.github.com>
2022-08-03 20:27:17 +0900

    ユーザー管理機能のテストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Last name is invalid. Input full-width characters" }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "First name is invalid. Input full-width characters" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Last name kana is invalid. Input full-width katakana characters" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "First name kana is invalid. Input full-width katakana characters" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana is invalid. Input full-width katakana characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana is invalid. Input full-width katakana characters" }
   validates :birth_day, presence: true
 end
